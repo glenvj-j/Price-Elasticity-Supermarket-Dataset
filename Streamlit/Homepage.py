b9b85ba264a631fe23cb1f080d7c4ce1b9b4a325
Glen Valencius <glen.valencius@gmail.com>
2025-05-28 14:49:38 +0700

    Update Homepage.py

--- Streamlit/Homepage.py
+++ Streamlit/Homepage.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-import request
+import requests
 
 st.set_page_config(
     page_title="Price Elasticity Calculator",
